b8302e38db24e60f95d5455739db6d8f0367908f
marodacco <48792073+marodacco@users.noreply.github.com>
2025-09-05 11:19:21 +0900

    Create main function to operate math

--- calculator.js
+++ calculator.js
@@ -14,3 +14,29 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+// create valuables for two numbers and an operator
+const numberA = 0;
+const numberB = 0;
+const mathOperator = "";
+
+// create main funcion to take numbers/operator and call one of the math functions
+function operate(num1, num2, math) {
+  let result = 0;
+
+  switch(math) {
+    case "add":
+      result = add(num1, num2);
+      break;
+    case "subtract":
+      result = subtract(num1, num2);
+      break;
+    case "multiply":
+      result = multiply(num1, num2);
+      break;
+    case "divide":
+      result = divide(num1, num2);
+  }
+
+  return result;
+}
